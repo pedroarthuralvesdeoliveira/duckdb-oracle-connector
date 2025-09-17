1b72229b752698ebf307f62dcb240730f5cee548
Pedro de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2025-09-17 14:42:40 -0300

    refactor: modularize code to separate by intention

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import duckdb
+from oracle_data_loader import connect_to_oracle, load_data_to_duckdb
+
 def main():
-    print("Hello from oracle-connector-to-duckdb!")
+    """Função principal para executar o fluxo de trabalho."""
+    sql = "SELECT * FROM PRODUTOS"
+    try:
+        with connect_to_oracle() as connection:
+            load_data_to_duckdb(connection, "products", sql)
+            
+        print(duckdb.sql("SELECT * FROM products"))
+    except oracledb.Error as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
